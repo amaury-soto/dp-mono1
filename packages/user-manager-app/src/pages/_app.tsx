17b7531c40177ec16a59c6b8674cff6c195aeeec
Amaury Soto Garay <amaury.soto@globant.com>
2023-03-31 14:29:20 -0500

    install react query and wrapper setting

--- packages/user-manager-app/src/pages/_app.tsx
+++ packages/user-manager-app/src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { ReactNode } from "react";
+//import { ReactQueryProvider } from "../lib/react-query";
+import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+const queryClient = new QueryClient();
+export default function App({ Component, pageProps }: AppProps): ReactNode {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
 }
