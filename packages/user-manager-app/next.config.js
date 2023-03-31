b5dc2cda8bb55046cad2f470a4b9deab98b656bb
Amaury Soto Garay <amaury.soto@globant.com>
2023-03-31 13:44:54 -0500

    install dotenv and settings to envs

--- packages/user-manager-app/next.config.js
+++ packages/user-manager-app/next.config.js
@@ -1,3 +1,12 @@
+const dotenv = require('dotenv')
+dotenv.config()
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config({ path: '.env.development' })
+} else {
+  dotenv.config()
+}
+//console.log('NODE_ENV',process.env.NODE_ENV)
+//console.log('API URL:', process.env.API_URL)
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
